4af6d7bc16c72faa616de3d5b6b2cbe70d73ec35
mariiana-lp <mariana.lopez.docs_221@gmail.com>
2022-03-26 18:58:12 -0500

    Logica de componente From

--- front/src/components/From.jsx
+++ front/src/components/From.jsx
@@ -1,11 +1,17 @@
 import React, { useState, Fragment } from 'react'
 import { connect } from 'react-redux';
+import { fetchRandom } from '../actions';
 
 
-const From = () => {// component stateless
+const From = (props) => {
+  const [state, setState] = useState();
+  const onSubmit = (e) => {
+    e.preventDefault();
+    props.dispatch(fetchRandom(state));
+  };
   
   return <div>
-    <form className='m-3'>
+    <form className='m-3' onSubmit={onSubmit}>
       <label>EL entero debe tener un valor entre</label>
       <input id="numeroInicial" 
         className='m-3' 
@@ -17,10 +23,21 @@ const From = () => {// component stateless
         type="text" 
         size = '8'/>
       <label>(ambos incluye un limite de ±1,000,000,000).</label>
+      <button className='button btn-primary m-3' 
+        type='submit'
+        disabled={props.loading}>Obtener Numero</button>
     </form>
   </div>
-  
+
 }
 
+  const stateMapToPros = state => {
+    return {
+      loading: state.view.loading
+    }
+  
+  }
 
-export default From;
+
+export default From
+//connect(stateMapToPros)(From)
--- front/src/components/Result.jsx
+++ front/src/components/Result.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react'
 import { connect } from 'react-redux';
+
 const Result = (props) => {
   
   return <div>
